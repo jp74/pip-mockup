79a548b895181719af2956e57a000ff848240754
Ashley Wilson <ash@jp74.com>
2014-05-21 11:12:35 +0100

    refactor setup file

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 from setuptools import setup, find_packages
 import os
-import django_mock as app
+import pip_mockup as app
 
 
 CLASSIFIERS = [
-    "Framework :: Django",  # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    "Framework :: Django",
 ]
 
 setup(
-    author="Ashley Wilson",
-    author_email="ash@jp74.com",
-    name="django_mock",
+    author="JP74",
+    author_email="mockup@sample.com",
+    name="pip_mockup",
     version=app.__version__,
     description="My test package",
     # Long desc will display on the PyPi page
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    url="https://github.com/CptLemming/django-mock",
-    download_url="https://github.com/CptLemming/django-mock/archive/master.zip",
+    url="https://github.com/jp74/pip_mockup",
+    download_url="https://github.com/jp74/pip_mockup/archive/master.zip",
     license="BSD",
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     install_requires=[
         'Django>=1.6',
     ],
+    tests_require=[
+        'pytest>=2.1'
+    ],
     packages=find_packages(exclude=["project", "project.*"]),
     include_package_data=True,
     zip_safe=True,
-    test_suite='run tests.main',
 )
-
-# pip install -e git+https://github.com/CptLemming/django-mock.git#egg=django_mock
